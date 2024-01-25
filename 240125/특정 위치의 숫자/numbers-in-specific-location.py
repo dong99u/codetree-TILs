3cf2514a51ddcaf1420dd34445ca708f007e28e4
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-01-25 12:05:30 +0900

    Title: 특정 위치의 숫자, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/특정 위치의 숫자/numbers-in-specific-location.py
+++ 240125/특정 위치의 숫자/numbers-in-specific-location.py
@@ -2,7 +2,8 @@ num_list = list(map(int, input().split()))
 
 sum_val = 0
 
-for val in num_list:
-    sum_val += val
+for idx, val in enumerate(num_list):
+    if idx == 2 or idx == 4 or idx == 9:
+        sum_val += val
 
 print(sum_val)
