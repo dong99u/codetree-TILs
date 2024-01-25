fcc2c9d32cff4bc613f3fade281b28e6c4580eb7
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-01-25 12:11:14 +0900

    Title: 배열의 특정 위치 찾기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240125/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,6 +3,6 @@ num_list = list(map(int, input().split()))
 even_sum = sum(num_list[1::2])
 
 lst = num_list[2::3]
-avg = sum(lst) / len(lst)
+avg = round(sum(lst) / len(lst), 1)
 
 print(even_sum, avg)
