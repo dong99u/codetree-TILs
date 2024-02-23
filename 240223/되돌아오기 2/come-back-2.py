60204c3f49dbdb77a96abaab32abf2b83f304c24
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-23 16:09:07 +0900

    Title: 되돌아오기 2, Time: 123ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240223/되돌아오기 2/come-back-2.py
@@ -0,0 +1,30 @@
+def is_home(x, y):
+    return x == 0 and y == 0
+
+instructions = input()
+
+# (x, y): 현재 위치
+x, y = 0, 0
+counter = 0
+
+# 북, 동, 남, 서 순서
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+dir_num = 0
+
+for instruction in instructions:
+    if instruction == 'R':
+        dir_num = (dir_num + 1) % 4
+    if instruction == 'L':
+        dir_num = (dir_num + 4 - 1) % 4
+    if instruction == 'F':
+        x += dx[dir_num]
+        y += dy[dir_num]
+
+    counter += 1
+
+    if is_home(x, y):
+        break
+
+print(counter)
