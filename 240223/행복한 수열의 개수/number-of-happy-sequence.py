d178f028558400fe52900d64ab53d1cf8d57ab6e
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-23 17:15:39 +0900

    Title: 행복한 수열의 개수, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240223/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,48 @@
+# n: 격자의 크기 n x n
+# m: 연속해야하는 숫자의 크기
+
+answer = 0
+
+n, m = map(int, input().split())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 각 행이 행복한 수열인지 확인하는 함수
+def happy_row():
+    global answer
+    for row in grid:
+        if sequence_check(row):
+            answer += 1
+
+# 각 열이 행복한 수열인지 확인하는 함수
+def happy_col():
+    global answer
+    columns = list(zip(*grid))
+    for column in columns:
+        if sequence_check(column):
+            answer += 1
+        
+
+
+# m개 이상 연속한지 확인하는 함수
+# m개 이상 연속하면 return True
+def sequence_check(sequence: list) -> bool:
+    count = 1
+    for i in range(1, len(sequence)):
+        if sequence[i - 1] == sequence[i]:
+            count += 1
+        else:
+            count = 1
+        
+    if count >= m:
+        return True
+    else:
+        return False
+
+happy_row()
+happy_col()
+
+print(answer)
