c5f498d339b78754aa055889a66ebac92bb6d4c3
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-23 18:18:03 +0900

    Title: 트로미노, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240223/트로미노/tromino.py
+++ 240223/트로미노/tromino.py
@@ -1,79 +1,65 @@
 # 각 모양별로 틀 안의 숫자의 합을 구하는 함수
 def get_sum_each_shape(row_s, col_s, row_e, col_e):
-    '''
+    """
     ㅁ
     ㅁㅁ
-    '''
-    sum_a = sum((
-        grid[row_s][col_s],
-        grid[row_e][col_s],
-        grid[row_e][col_e]
-    ))
+    """
+    sum_a = sum((grid[row_s][col_s], grid[row_e][col_s], grid[row_e][col_e]))
 
-    '''
+    """
     ㅁㅁ
     ㅁ
-    '''
-    sum_b = sum((
-        grid[row_s][col_s],
-        grid[row_s][col_e],
-        grid[row_e][col_s]
-    ))
+    """
+    sum_b = sum((grid[row_s][col_s], grid[row_s][col_e], grid[row_e][col_s]))
 
-    '''
+    """
     ㅁㅁ
      ㅁ
-    '''
-    sum_c = sum((
-        grid[row_s][col_s],
-        grid[row_s][col_e],
-        grid[row_e][col_e]
-    ))
+    """
+    sum_c = sum((grid[row_s][col_s], grid[row_s][col_e], grid[row_e][col_e]))
 
-    '''
+    """
      ㅁ
     ㅁㅁ
-    '''
-    sum_d = sum((
-        grid[row_s][col_e],
-        grid[row_e][col_s],
-        grid[row_e][col_e]
-    ))
+    """
+    sum_d = sum((grid[row_s][col_e], grid[row_e][col_s], grid[row_e][col_e]))
 
     return max(sum_a, sum_b, sum_c, sum_d)
 
+
+def get_sum_row(row, col_s, col_e):
+    return sum(grid[row][col_s : col_e + 1])
+
+
+def get_sum_col(row_s, row_e, col):
+    return sum([grid[row][col] for row in range(row_s, row_e + 1)])
+
+
 n, m = map(int, input().split())
 
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-max_sum = 0
+answer = 0
 
-'''
-ㅁㅁㅁ
-'''
-for row in grid:
-    sum_val = sum(row)
-    max_sum = max(max_sum, sum_val)
-
-'''
-ㅁ
-ㅁ
-ㅁ
-'''
-for col in list(zip(*grid)):
-    sum_val = sum(col)
-    max_sum = max(max_sum, sum_val)
+for row in range(n):
+    for col in range(m):
+        # 1 x 3 범위 안
+        if col + 2 < m:
+            row_sum = get_sum_row(row, col, col + 2)
+            answer = max(answer, row_sum)
+        # 3 x 1 범위 안
+        if row + 2 < n:
+            col_sum = get_sum_col(row, row + 2, col)
+            answer = max(answer, col_sum)
 
 
 for row in range(n):
     for col in range(m):
-        # 범위 안에 있으면
+
+        # 2 x 2사각형 범위 안
         if row + 1 <= 2 and col + 1 <= 2:
             # 그 2x2 사각형 안에서 최대 크기를 구함
             max_sum_in_square = get_sum_each_shape(row, col, row + 1, col + 1)
-            max_sum = max(max_sum, max_sum_in_square)
+            answer = max(answer, max_sum_in_square)
 
-print(max_sum)
+print(answer)
