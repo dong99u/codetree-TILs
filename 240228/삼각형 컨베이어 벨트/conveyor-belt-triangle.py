c51bc1afd75e3752d803ac02e9e1c13385438694
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-28 00:32:35 +0900

    Title: 삼각형 컨베이어 벨트, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240228/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,15 @@
+n, t = tuple(map(int, input().split()))
+
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
+l3 = list(map(int, input().split()))
+
+sum_list = l1 + l2 + l3
+
+for _ in range(t):
+    temp = sum_list.pop(-1)
+    sum_list.insert(0, temp)
+    
+print(*sum_list[:n])
+print(*sum_list[n:n+3])
+print(*sum_list[n+3:])
