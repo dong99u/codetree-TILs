2f01b21a95529fc22ec2cb4cc3d4d81c5aa97ac0
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-28 15:40:33 +0900

    Title: 최단 Run Length 인코딩, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240228/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,18 +1,28 @@
-A = list(input())
+A = input()
 
 answer = float("inf")
 
-for _ in range(len(A)):
-    encoded_str = ""
-    count = 1
-    for i in range(1, len(A)):
-        if A[i - 1] == A[i]:
-            count += 1
-        elif A[i - 1] != A[i] or i == len(A) - 1:
-            encoded_str += str(A[i - 1]) + str(count)
-            count = 1
-        
 
-    answer = min(answer, len(encoded_str))
+for _ in range(len(A)):
+    encoded = ""
+    count = 1
+    curr_char = A[0]
+
+    for target_char in A[1:]:
+        if target_char == curr_char:
+            count += 1
+        else:
+            encoded += curr_char
+            encoded += str(count)
+
+            curr_char = target_char
+            count = 1
+
+    encoded += curr_char
+    encoded += str(count)
+
+    answer = min(len(encoded), answer)
+
+    A = A[-1] + A[:-1]
 
 print(answer)
