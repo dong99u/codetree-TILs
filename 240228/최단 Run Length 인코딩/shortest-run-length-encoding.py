04baed573831bc8a5f9c6e879fec625bc2415f36
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-28 15:21:58 +0900

    Title: 최단 Run Length 인코딩, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240228/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240228/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,16 +1,18 @@
 A = list(input())
 
-answer = float('inf')
+answer = float("inf")
 
 for _ in range(len(A)):
-    encoded_str = ''
+    encoded_str = ""
     count = 1
     for i in range(1, len(A)):
-        if A[i -1] == A[i]:
+        if A[i - 1] == A[i]:
             count += 1
-        else:
+        elif A[i - 1] != A[i] or i == len(A) - 1:
             encoded_str += str(A[i - 1]) + str(count)
-    
+            count = 1
+        
+
     answer = min(answer, len(encoded_str))
 
 print(answer)
