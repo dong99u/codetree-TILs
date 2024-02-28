f766269b4e5148e1f57727f8e0fca453ffd612ff
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-28 17:49:10 +0900

    Title: 떨어지는 1자 블록, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240228/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240228/떨어지는 1자 블록/falling-horizontal-block.py
@@ -10,23 +10,21 @@ grid = [
 ]
 
 # 다음 행의 모든 줄이 0이면 True
-# row: 다음 행
-def can_land(row) -> bool:
+def all_blank(row) -> bool:
     global grid, m, k
 
     return all(value == 0 for value in grid[row][k:k + m])
 
-def land(row):
-    global grid, m ,k
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1):
+            return row
+    return n - 1
 
-    for col in range(k, k + m):
-        grid[row][col] = 1
+target_row = get_target_row()
 
-for row in range(n):
+for col in range(k, k + m):
+    grid[target_row][col] = 1
 
-    if not can_land(row):
-        land(row - 1)
-        break
-    
-for row in grid:
-    print(*row)
+for elem in grid:
+    print(*elem)
