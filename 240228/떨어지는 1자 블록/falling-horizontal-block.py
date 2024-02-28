ea5f8d7be4634199aa297570319ab1e1dd045de7
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-28 17:41:24 +0900

    Title: 떨어지는 1자 블록, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240228/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,32 @@
+import sys
+
+# 입력
+n, m, k = tuple(map(int, sys.stdin.readline().split()))
+k -= 1
+
+grid = [
+    list(map(int, sys.stdin.readline().split()))
+    for _ in range(n)
+]
+
+# 다음 행의 모든 줄이 0이면 True
+# row: 다음 행
+def can_land(row) -> bool:
+    global grid, m, k
+
+    return all(value == 0 for value in grid[row][k:k + m])
+
+def land(row):
+    global grid, m ,k
+
+    for col in range(k, k + m):
+        grid[row][col] = 1
+
+for row in range(1, n):
+
+    if not can_land(row):
+        land(row - 1)
+        break
+    
+for row in grid:
+    print(*row)
