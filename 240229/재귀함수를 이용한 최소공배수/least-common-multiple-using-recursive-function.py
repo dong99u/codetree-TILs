f8ced59a8671c2085885c41dfdabbb37a0158eb9
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-02-29 19:32:17 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240229/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,24 @@
+n = int(input())
+l = list(map(int, input().split()))
+
+def gcd(a, b):
+
+    if b == 0:
+        return a
+
+    else:
+        return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
+def f(n):
+    if n == 1:
+        return lcm(l[0], l[1])
+
+    return lcm(f(n - 1), l[n])
+
+result = int(f(n - 1))
+
+print(result)
