f18dfd03eece30922e518e0b326e531e9803ac99
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-06 05:10:21 +0900

    Title: 원점으로부터의 거리, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+points = [
+    (int(x), int(y), idx + 1) for idx, (x, y) in enumerate(input().split() for _ in range(N))
+]
+
+
+points.sort(key=lambda x: abs(x[0] + x[1]))
+
+for point in points:
+    print(point[-1])
