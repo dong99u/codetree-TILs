ebd9ffe2fe280bb975ff943151f8b9866a166d3b
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-06 13:40:41 +0900

    Title: 아름다운 수, Time: 880ms, Memory: 28MB, Status: Passed - CodeTree

--- 240306/아름다운 수/beautiful-number.py
+++ 240306/아름다운 수/beautiful-number.py
@@ -4,17 +4,32 @@ sequence = []
 
 answer = 0
 
+
 def check_happy():
     global answer
-    
-    arr = [0] * 4
-    
-    for seq in sequence:
-        arr[seq - 1] += 1
 
-    # 2의 개수가 2의 배수라면 행복
-    if all([arr[1] % 2 == 0, arr[2] % 3 == 0, arr[3] % 4 == 0]):
+    cnt = 1
+    curr_num = sequence[0]
+
+    arr = []
+    for seq in sequence[1:]:
+        if curr_num == seq:
+            cnt += 1
+
+        else:
+            arr.append((curr_num, cnt))
+            cnt = 1
+        curr_num = seq
+
+    arr.append((curr_num, cnt))
+
+    if all([True if cnt % num == 0 else False for num, cnt in arr]):
         answer += 1
+
+    # # 2의 개수가 2의 배수라면 행복
+    # if all([arr[1] % 2 == 0, arr[2] % 3 == 0, arr[3] % 4 == 0]):
+    #     answer += 1
+
 
 def f(idx):
 
