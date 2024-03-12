a40e68860a7ce239e51e6e32433872b4384013a4
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-12 22:45:55 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 157ms, Memory: 55MB, Status: Wrong Answer - CodeTree

---
+++ 240312/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,34 @@
+def make_square(x1, y1, x2, y2, value):
+    global grid
+
+    for x in range(x1 + OFFSET, x2 + OFFSET):
+        for y in range(y1 + OFFSET, y2 + OFFSET):
+
+            grid[x][y] = value
+
+
+MAX_XY = 2000
+OFFSET = 1000
+
+grid = [[0] * MAX_XY for _ in range(MAX_XY)]
+
+
+make_square(*tuple(map(int, input().split())), 1)
+make_square(*tuple(map(int, input().split())), 0)
+
+min_x, min_y, max_x, max_y = MAX_XY, MAX_XY, -1, -1
+
+for x in range(MAX_XY):
+    for y in range(MAX_XY):
+
+        if grid[x][y] == 1:
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+
+width = max_y - min_y + 1
+height = max_x - min_x + 1
+
+print(width * height)
