3f3f6652f4b96f3175cfa8685bf75203268a039c
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-12 23:41:10 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, t = tuple(map(int, input().split()))
+
+sequence = tuple(map(int, input().split()))
+
+cnt = 0
+answer = 0
+
+for i in range(1, n):
+    
+    if t <= sequence[i - 1] and t <= sequence[i] and sequence[i - 1] < sequence[i]:
+        cnt += 1
+    else:
+        cnt = 1
+
+    answer = max(answer, cnt)
+
+print(answer)
