d7917a961c8a1cbb869dba1fa64e8e4b3902c671
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-14 12:00:02 +0900

    Title: 체크판위에서 2, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240314/체크판위에서 2/on-the-checkboard-2.py
+++ 240314/체크판위에서 2/on-the-checkboard-2.py
@@ -8,14 +8,17 @@ grid = [
     for _ in range(r)
 ]
 answer = 0
-for x1 in range(1, c - 2):
-    for y1 in range(1, r - 2):
+if grid[0][0] == grid[r - 1][c - 1]:
+    print(answer)
+else:
+    for x1 in range(1, c - 2):
+        for y1 in range(1, r - 2):
 
-        if grid[x1][y1] == 'B':
+            if grid[x1][y1] == 'B':
 
-            for x2 in range(x1 + 1, c):
-                for y2 in range(y1 + 1, r):
-                    if grid[x2][y2] == 'W':
-                        answer += 1
+                for x2 in range(x1 + 1, c):
+                    for y2 in range(y1 + 1, r):
+                        if grid[x2][y2] == 'W':
+                            answer += 1
 
-print(answer)
+    print(answer)
