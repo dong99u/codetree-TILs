065bf2a9e4c85040a54daf6c10501477520a0461
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-14 09:55:34 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,37 @@
+n, m = tuple(map(int, input().split()))
+
+grid = [
+    [0] * m
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def already_filled(x, y):
+    return grid[x][y] != 0
+
+def simulate():
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    # 시작 위치
+    x, y = 0, 0
+    # 시작 방향
+    dir_num = 0
+
+    for num in range(1, n * m + 1):
+        grid[x][y] = num
+        nx, ny = x + dx[dir_num], y + dy[dir_num]
+
+        if not in_range(nx, ny) or already_filled(nx, ny):
+            dir_num = (dir_num + 1) % 4
+            nx, ny = x + dx[dir_num], y + dy[dir_num]
+        
+        x, y = nx, ny
+
+    
+simulate()
+
+for row in grid:
+    print(*row)
