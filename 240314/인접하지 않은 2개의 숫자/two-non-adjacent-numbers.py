4e94cd018e5fd343f2e842e98d1bba0d58fc7727
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-14 17:53:42 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240314/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+answer = 0
+
+for i in range(n - 2):
+    for j in range(i + 2, n):
+
+        answer = max(answer, arr[i] + arr[j])
+
+print(answer)
