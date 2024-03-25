6d7a9ad38f34935e21334f06f4c85e5375393c7c
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-25 10:53:09 +0900

    Title: 아름다운 수열 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240325/아름다운 수열 2/beautiful-sequence-2.py
+++ 240325/아름다운 수열 2/beautiful-sequence-2.py
@@ -6,7 +6,7 @@ B = sorted(list(map(int, input().split())))
 answer = 0
 for i in range(n - 2):
 
-    a_seq = sorted(A[i : i + 3])
+    a_seq = sorted(A[i : i + m])
     if a_seq == B:
         answer += 1
 
