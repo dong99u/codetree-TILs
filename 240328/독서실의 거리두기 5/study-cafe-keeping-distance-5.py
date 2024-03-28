bfa740289a7db3d79cf1a122bf619925dac93d36
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-03-28 10:51:29 +0900

    Title: 독서실의 거리두기 5, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240328/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+arr = input()
+
+answer = 0
+
+for seat_num in range(n):
+
+    if arr[seat_num] == "1":
+        continue
+
+    if arr[seat_num] == "0":
+        d1, d2 = 0, 0
+
+        for i in range(seat_num - 1, -1, -1):
+            if arr[i] == "1":
+                d1 = seat_num - i
+                break
+
+        for i in range(seat_num + 1, n):
+            if arr[i] == "1":
+                d2 = i - seat_num
+                break
+
+        min_dist = min(d1, d2)
+
+        answer = max(answer, min_dist)
+
+print(answer)
