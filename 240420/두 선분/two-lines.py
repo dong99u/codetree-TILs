5a95c296fa389c39607f654f8cddcef99a9f94a0
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-04-20 16:32:17 +0900

    Title: 두 선분, Time: 187ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/두 선분/two-lines.py
+++ 240420/두 선분/two-lines.py
@@ -1,8 +1,12 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-answer = False
-if x3 <= x2 or x4 <= x1:
-    answer = True
+answer = True
+
+# 안 겹치는 경우
+if (x1 < x3 and x2 < x3):
+    answer = False
+elif (x3 < x1 and x4 < x1):
+    answer = False
 
 if answer:
     print('intersecting')
