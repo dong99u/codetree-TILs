d323fcc364f42499b86139909d5a6d7f7d8907ec
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-06-24 20:22:49 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240624/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int answer = solution(n, m, matrix);
+
+        System.out.println(answer);
+        
+    }
+
+    public static int solution(int n, int m, int[][] matrix) {
+        
+        // 행 방향부터 
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j - 1] == matrix[i][j]) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+
+                if (cnt >= m) {
+                    result++;
+                    break;
+                }
+            }
+        }
+    
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 1; j < n; j++) {
+
+                if (matrix[j - 1][i] == matrix[j][i]) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+
+                if (cnt >= m) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        
+        return result;
+    }
+
+
+}
