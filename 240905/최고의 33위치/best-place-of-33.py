01d4b52b4f9a058051280308bb4adaff063577de
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-09-05 12:13:47 +0900

    Title: 최고의 33위치, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240905/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 3x3 격자의 코인의 값을 가져오는 함수
+def get_num_of_gold(row_s, col_s):
+    result = 0
+    for row in range(row_s, row_s + 2 + 1):
+        for col in range(col_s, row_s + 2 + 1):
+            
+            if row >= n or col >= n:
+                return 
+
+            result += grid[row][col]
+
+    return result
+
+answer = 0
+
+for row in range(n):
+    for col in range(n):
+
+        result = get_num_of_gold(row, col)
+        if result:
+            answer = max(answer, result)
+        
+
+print(answer)
