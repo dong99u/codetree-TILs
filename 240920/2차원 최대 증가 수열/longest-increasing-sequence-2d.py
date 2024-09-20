56b9f08a8705f6fcef9b12288f0e043806472000
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-09-20 11:00:17 +0900

    Title: 2차원 최대 증가 수열, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240920/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240920/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -23,6 +23,9 @@ for x in range(1, n):
         for i in range(x):
             for j in range(y):
 
+                if dp[i][j] == 0:
+                    continue
+
                 if grid[x][y] > grid[i][j]:
                     dp[x][y] = max(dp[i][j] + 1, dp[x][y])
 answer = MIN_INT
