d46034563394b7bc82480bc5c0f11338abc73e73
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-09 16:16:43 +0900

    Title: 정수 찾기, Time: 5036ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

---
+++ 241009/정수 찾기/find-an-integer.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+a = list(map(int, input().split()))
+
+m = int(input().rstrip())
+
+b = list(map(int, input().split()))
+
+for elem in b:
+    if elem in a:
+        print(1)
+    else:
+        print(0)
