583bfc79fa1496f4aff03ee8a2906dc8f88cfda8
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-09 20:11:10 +0900

    Title: 대칭 차집합, Time: 327ms, Memory: 66MB, Status: Passed - CodeTree

---
+++ 241009/대칭 차집합/symmetric-difference-set.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+
+c = a - b
+d = b - a
+
+print(len(c) + len(d))
