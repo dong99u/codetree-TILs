6346924545bfa23f4f11ef47f97beac45361dd14
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-10 13:20:47 +0900

    Title: 정수 명령어, Time: 279ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241010/정수 명령어/Integer-command.py
@@ -0,0 +1,29 @@
+from sortedcontainers import SortedList
+
+sl = SortedList()
+
+
+
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    for _ in range(k):
+
+        orders = input().split()
+
+        order = orders[0]
+        num = int(orders[1])
+
+        if order == 'I':
+            sl.add(num)
+        elif order == 'D':
+            if sl:
+                if num == 1:
+                    sl.pop()
+                else:
+                    sl.pop(0)
+
+    if sl:
+        print(sl[-1], sl[0])
+    else:
+        print('EMPTY')
