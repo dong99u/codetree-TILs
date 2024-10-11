c24e9f4e18426a2507a616a8b80936f631e481a8
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-11 12:14:34 +0900

    Title: 마지막으로 남은 숫자, Time: 291ms, Memory: 30MB, Status: Passed - CodeTree

--- 241011/마지막으로 남은 숫자/last-remaining-number.py
+++ 241011/마지막으로 남은 숫자/last-remaining-number.py
@@ -10,9 +10,14 @@ heap = list(map(lambda x: -int(x), input().split()))
 heapq.heapify(heap)
 
 while len(heap) > 1:
-    a = heapq.heappop(heap) # 가장 큰 수
-    b = heapq.heappop(heap) # 두 번째로 큰 수
+    a = -heapq.heappop(heap) # 가장 큰 수
+    b = -heapq.heappop(heap) # 두 번째로 큰 수
 
-    heapq.heappush(heap, a - b)
+    diff = a - b
+    if diff != 0:
+        heapq.heappush(heap, -diff)
 
-print(-heap[0])
+if len(heap) == 1:
+    print(-heapq.heappop(heap))
+else:
+    print(-1)
