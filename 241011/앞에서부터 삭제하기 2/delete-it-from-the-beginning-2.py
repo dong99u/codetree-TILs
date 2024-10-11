7ad487c6500c49822f4492b795ee39ff49feb60a
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-11 15:17:44 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5094ms, Memory: 41MB, Status: Time Limit Exceed - CodeTree

--- 241011/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241011/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -5,17 +5,16 @@ input = sys.stdin.readline
 
 n = int(input().rstrip())
 
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
+heap = []
 answer = 0
+heapq.heappush(heap, arr[-1])
+for i in range(n - 2, 0, -1):
+    heapq.heappush(heap, arr[i])
+    others_without_min_val = heap[1:]
+    avg = round(sum(others_without_min_val) / len(others_without_min_val), 2)
 
-for k in range(1, n - 2 + 1):
-    heap = nums[k:]
-    heapq.heapify(heap)
-
-    heapq.heappop(heap) # 가장 작은 수 하나를 뺌
-
-    avg = round(sum(heap) / len(heap), 2)
     answer = max(answer, avg)
 
 print(f'{answer:.2f}')
