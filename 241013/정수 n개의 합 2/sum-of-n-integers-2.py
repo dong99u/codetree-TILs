ea722ba10d19f3f7f5eb59e7af1060f6c6ef37f0
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-13 15:39:11 +0900

    Title: 정수 n개의 합 2, Time: 142ms, Memory: 32MB, Status: Passed - CodeTree

--- 241013/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 241013/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-arr = list(map(int, input().split()))
-prefix_sum = [0] * n
-prefix_sum[0] = arr[0]
+arr = [0] + list(map(int, input().split()))
+prefix_sum = [0] * (n + 1)
+prefix_sum[1] = arr[1]
 
 for i in range(1, n):
     prefix_sum[i] = prefix_sum[i - 1] + arr[i]
 
 max_num = 0
 
-for i in range(k, n):
+for i in range(k + 1, n):
     num = prefix_sum[i] - prefix_sum[i - k]
     max_num = max(max_num, num)
 
