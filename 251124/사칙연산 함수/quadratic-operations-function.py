67e27c7628a27d92111974f7dcb6cac34a3e4b2e
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-11-24 13:32:54 +0900

    Title: 사칙연산 함수, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251124/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,29 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+
+def add(a, c):
+    return a + b
+
+def minus(a, c):
+    return a - c
+
+def multi(a, c):
+    return a * c
+
+def devide(a, c):
+    return a // c
+
+if o in '+-/*':
+    if o == '+':
+        print(a, o, c, '=', add(a, c))
+    elif o == '-':
+        print(a, o, c, '=', minus(a, c))
+    elif o == '*':
+        print(a, o, c, '=', multi(a, c))
+    else:
+        print(a, o, c, '=', devide(a, c))
+else:
+    print(False)
