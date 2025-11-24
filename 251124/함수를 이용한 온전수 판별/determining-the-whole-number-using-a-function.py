6d8679858c75cc6c15cf938e8b919debb86e2627
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-11-24 13:39:25 +0900

    Title: 함수를 이용한 온전수 판별, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 251124/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -3,7 +3,7 @@ a, b = map(int, input().split())
 # Please write your code here.
 
 def check(n):
-    return n % 2 == 0 and n // 10 == 5 and (n % 3 == 0 and n % 9 != 0)
+    return not (n % 2 == 0 or n % 10 == 5 or (n % 3 == 0 and n % 9 != 0))
 
 answer = 0
 for n in range(a, b + 1):
