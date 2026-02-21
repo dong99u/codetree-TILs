0edacaa6ba287c4ee4b040f8aaaa48fc75245001
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-02-21 15:08:52 +0900

    Title: 최단 Run Length 인코딩, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260221/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+A = input()
+
+# Please write your code here.
+def check():
+    pass
+
+q = deque(A)
+
+answer = 1e9
+
+for _ in range(len(A) - 1):
+    cnt = 1
+    result = []
+    for i in range(1, len(A)):
+        if q[i - 1] == q[i]:
+            cnt += 1
+        else:
+            result.append(q[i - 1])
+            result.append(cnt)
+            cnt = 1
+    result.append(q[len(A) - 1])
+    result.append(cnt)
+
+    answer = min(answer, len(''.join(map(str, result))))
+
+    q.rotate(1)
+
+print(answer)
