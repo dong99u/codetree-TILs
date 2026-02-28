cd25d941d9c515d4be46b619c11601e127422345
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-02-28 20:37:25 +0900

    Title: 벽이 없는 충돌 실험, Time: 4064ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

--- 260228/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 260228/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,42 +1,19 @@
 from collections import defaultdict
 
 t = int(input())
-
-DIST = 1
-
-class Marble:
-    def __init__(self, x, y, d, weight, num):
-        self.x = x
-        self.y = y
-        self.d = d
-        self.weight = weight
-        self.num = num
-
-    def move(self):
-        self.x += dxs[self.d] * DIST
-        self.y += dys[self.d] * DIST
-
- # 우 상 좌 하
 dxs = [1, 0, -1, 0]
 dys = [0, 1, 0, -1]
+mapper = {'R': 0, 'U': 1, 'L': 2, 'D': 3}
 
 for _ in range(t):
     n = int(input())
-
     grid = defaultdict(list)
-
-    mapper = {
-        'R': 0,
-        'U': 1,
-        'L': 2,
-        'D': 3,
-    }
 
     for i in range(n):
         xi, yi, wi, di = input().split()
         xi, yi, wi = int(xi), int(yi), int(wi)
         di = mapper[di]
-        grid[(xi * 2, yi * 2)].append((xi * 2, yi * 2, di, wi, i + 1))
+        grid[(xi*2, yi*2)].append((xi*2, yi*2, di, wi, i+1))
 
     answer = -1
     for time in range(1, 4003):
