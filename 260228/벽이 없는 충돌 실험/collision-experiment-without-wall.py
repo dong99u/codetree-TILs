3686e06d980e4e376269a3a353cc2b7863669114
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-02-28 20:37:04 +0900

    Title: 벽이 없는 충돌 실험, Time: 4079ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

--- 260228/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 260228/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -34,30 +34,28 @@ for _ in range(t):
 
     for i in range(n):
         xi, yi, wi, di = input().split()
-        xi, yi, wi = map(int, (xi, yi, wi))
+        xi, yi, wi = int(xi), int(yi), int(wi)
         di = mapper[di]
-        marble = Marble(xi * 2, yi * 2, di, wi, i + 1)
-        grid[(xi * 2, yi * 2)].append(marble)
+        grid[(xi * 2, yi * 2)].append((xi * 2, yi * 2, di, wi, i + 1))
 
     answer = -1
-    for time in range(4002):
+    for time in range(1, 4003):
         new_grid = defaultdict(list)
-        # 구슬이 1개 이하면 종료
-        total_marbles = sum(len(v) for v in grid.values())
-        if total_marbles <= 1:
+        total = sum(len(v) for v in grid.values())
+        if total <= 1:
             break
+
         for marbles in grid.values():
-            for marble in marbles:
-                marble.move()
-                new_grid[(marble.x, marble.y)].append(marble)
+            for x, y, d, w, num in marbles:
+                nx, ny = x + dxs[d], y + dys[d]
+                new_grid[(nx, ny)].append((nx, ny, d, w, num))
 
         for pos, marbles in new_grid.items():
             if len(marbles) > 1:
-                answer = time + 1
-                final_marble = max(marbles, key=lambda m: (m.weight, m.num))
-                new_grid[pos] = [final_marble]
+                answer = time
+                best = max(marbles, key=lambda m: (m[3], m[4]))
+                new_grid[pos] = [best]
 
         grid = new_grid
-
 
     print(answer)
