5b899e69710c2aed770457016c420674d29917ba
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-04 14:53:32 +0900

    Title: XOR 결과 최대 만들기, Time: 51ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260304/XOR 결과 최대 만들기/max-of-xor.py
+++ 260304/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,13 +1,25 @@
+from collections import defaultdict
+
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 # Please write your code here.
-
+memo = defaultdict(int)
 def backtrack(idx, curr, count):
+    state = (idx, count)
+    if state in memo:
+        return memo[state]
     if count == m:
+        memo[state] = curr
         return curr
     if idx == n:
         return count
-    return max(backtrack(idx + 1, curr ^ A[idx], count + 1), backtrack(idx + 1, curr, count))
+
+    result = max(backtrack(idx + 1, curr ^ A[idx], count + 1), backtrack(idx + 1, curr, count))
+
+    memo[state] = result
+
+    return result
+
 
 print(backtrack(0, 0, 0))
