914bc4b3047d62273231df53df5be8b28bc0342c
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-04 10:13:10 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260304/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -0,0 +1,31 @@
+n = int(input())
+num = [list(map(int, input().split())) for _ in range(n)]
+move_dir = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+
+# Please write your code here.
+dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
+dys = [0, 1, 1, 1, 0, -1, -1, -1]
+
+x, y = r - 1, c - 1
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def backtrack(x, y):
+    result = 0
+    i = 1
+    while in_range(x, y):
+        d = move_dir[x][y] - 1
+        nx, ny = x + dxs[d] * i, y + dys[d] * i
+        if not in_range(nx, ny):
+            return result
+        if num[nx][ny] > num[x][y]:
+            result = max(result, 1 + backtrack(nx, ny))
+        i += 1
+
+    return result
+
+answer = backtrack(x, y)
+
+print(answer)
