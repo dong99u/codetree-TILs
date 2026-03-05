1f61bb6d0b4ddafbb37d1039f3cfe0a77bba0a49
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-05 11:43:42 +0900

    Title: 외판원 순회, Time: 55ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260305/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,27 @@
+n = int(input())
+A = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+INF = float('inf')
+visited = [False] * n
+visited[0] = True
+
+def backtrack(curr, acc, count):
+    if count == n:
+        return acc + A[curr][0]
+
+    result = INF
+    for nxt in range(1, n):
+        if not visited[nxt]:
+            next_dist = acc + A[curr][nxt]
+            if next_dist > result:
+                continue
+            visited[nxt] = True
+            result = min(result, backtrack(nxt, next_dist, count + 1))
+            visited[nxt] = False
+
+    return result
+
+answer = backtrack(0, 0, 1)
+
+print(answer)
