73ffbf87d6d7cc07613c3b1d811b95df53bb66cc
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-05 15:08:13 +0900

    Title: 마을 구분하기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260305/마을 구분하기/seperate-village.py
@@ -0,0 +1,36 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+visited = [[False] * n for _ in range(n)]
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
+
+def dfs(x, y):
+    count = 1
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if not in_range(nx, ny):
+            continue
+        if grid[nx][ny] == 0:
+            continue
+        if visited[nx][ny]:
+            continue
+        visited[nx][ny] = True
+        count += dfs(nx, ny)
+    return count
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+results = []
+for x in range(n):
+    for y in range(n):
+        if grid[x][y] == 1 and not visited[x][y]:
+            visited[x][y] = True
+            results.append(dfs(x, y))
+
+results.sort()
+print(len(results))
+for num in results:
+    print(num)
