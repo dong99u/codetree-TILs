9408ec1f426615a3565d095385e676a2871418be
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-05 13:13:33 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260305/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,31 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+visited = [False] * n
+INF = float('inf')
+
+def backtrack(row, min_val):
+    '''
+    row: 현재 노드가 위치한 row의 인덱스
+    acc: 이전까지 선택한 값들의 합
+    '''
+
+    if row == n:
+        return min_val
+
+    result = 0
+    for col in range(n):
+        if visited[col]:
+            continue
+        visited[col] = True
+        if min_val > grid[row][col]:
+            min_val = grid[row][col]
+        result = max(result, backtrack(row + 1, min_val))
+        visited[col] = False
+
+    return result
+
+answer = backtrack(0, INF)
+
+print(answer)
