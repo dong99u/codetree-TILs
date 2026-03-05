b77bb4792a04637fb43eca7e781dfd321b128f35
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-03-05 12:52:47 +0900

    Title: 외판원 순회, Time: 242ms, Memory: 22MB, Status: Passed - Codetree

--- 260305/외판원 순회/traveling-salesman-problem.py
+++ 260305/외판원 순회/traveling-salesman-problem.py
@@ -8,11 +8,15 @@ visited[0] = True
 
 def backtrack(curr, acc, count):
     if count == n:
+        if A[curr][0] == 0:
+            return INF
         return acc + A[curr][0]
 
     result = INF
     for nxt in range(1, n):
         if not visited[nxt]:
+            if A[curr][nxt] == 0:
+                continue
             next_dist = acc + A[curr][nxt]
             if next_dist > result:
                 continue
