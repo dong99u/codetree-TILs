96b8ed478209e84c7bc8285c4530c5e1201a30a9
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-04-20 10:59:23 +0900

    Title: 정수 명령 처리 6, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/정수 명령 처리 6/process-numeric-commands-6.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String order = st.nextToken();
+
+			switch (order) {
+				case "push":
+					priorityQueue.add(Integer.parseInt(st.nextToken()));
+					break;
+
+				case "pop":
+					System.out.println(priorityQueue.poll());
+					break;
+
+				case "size":
+					System.out.println(priorityQueue.size());
+					break;
+
+				case "empty":
+					System.out.println(priorityQueue.isEmpty() ? 1 : 0);
+					break;
+
+				case "top":
+					System.out.println(priorityQueue.peek());
+					break;
+
+
+			}
+		}
+	}
+}
