f38da9237f65e474bb163c530bbc947284366737
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-04-20 10:11:02 +0900

    Title: hashset 기본, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/hashset 기본/hashset-basic.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		HashSet<Integer> hashSet = new HashSet<>();
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String order = st.nextToken();
+
+			switch (order) {
+				case "add":
+					hashSet.add(Integer.parseInt(st.nextToken()));
+					break;
+				case "remove":
+					hashSet.remove(Integer.parseInt(st.nextToken()));
+					break;
+				case "find":
+					boolean result = hashSet.contains(Integer.parseInt(st.nextToken()));
+					System.out.println(result);
+					break;
+			}
+		}
+	}
+}
