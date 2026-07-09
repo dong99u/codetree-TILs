9d7f9cfeabf79d75e2cffe5e56b533c0ee0f4cac
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-07-09 09:14:22 +0900

    [Passed] 부분수열 여부 판단하기 (248ms, 18MB)

---
+++ trail5/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[n];
+        int[] B = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean isPossible = true;
+        int i = 0;
+        for (int j = 0; j < m; j++) {
+            while (i < n && A[i] != B[j])
+                i++;
+            if (i == n) {
+                isPossible = false;
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println(isPossible == true ? "Yes" : "No");
+    }
+}
