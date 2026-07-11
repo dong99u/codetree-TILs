28a594bf1ce749ebf07f625e1d22eda9a44141e7
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-07-11 18:58:59 +0900

    [Passed] 중복되지 않는 가장 긴 문자열 (91ms, 9MB)

--- trail5/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.java
+++ trail5/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.java
@@ -7,25 +7,17 @@ public class Main {
         char[] chars = br.readLine().toCharArray();
         int n = chars.length;
 
-        char c = 'a';
         int[] arr = new int['z' - 'a' + 1];
-        arr[chars[0] - 'a']++;
         int answer = 0;
-        int j = 0;
+        int j = -1;
         for (int i = 0; i < n; i++) {
-            while (j + 1 < n && arr[chars[j] - 'a'] <= 1) {
+            while (j + 1 < n && arr[chars[j + 1] - 'a'] < 1) {
                 arr[chars[j + 1] - 'a']++;
                 j++;
             }
-            if (arr[chars[i] - 'a'] <= 1) {
-                if (arr[chars[j] - 'a'] > 1)
-                    answer = Math.max(answer, j - i);
-                else if (j == n - 1)
-                    answer = Math.max(answer, j - i + 1);
-            }
+            answer = Math.max(answer, j - i + 1);
             arr[chars[i] - 'a']--;
         }
-
         System.out.println(answer);
     }
 }
