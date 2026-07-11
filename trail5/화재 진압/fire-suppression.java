bf354c5e41d5f2d7e81d03389263a42fd6917a31
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2026-07-11 21:34:15 +0900

    [Passed] 화재 진압 (438ms, 24MB)

--- trail5/화재 진압/fire-suppression.java
+++ trail5/화재 진압/fire-suppression.java
@@ -2,59 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static final int MAX_IDX = (int)1e9 + 1;
+    static int n;
+    static int m;
+    static int[] fires;
+    static int[] stations;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        int[] fires = new int[n];
-        int[] stations = new int[m];
+        fires = new int[n];
+        stations = new int[m];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             fires[i] = Integer.parseInt(st.nextToken());
-            treeSet.add(fires[i]);
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
             stations[i] = Integer.parseInt(st.nextToken());
-            treeSet.add(stations[i]);
         }
 
-        HashMap<Integer, Integer> mapper = new HashMap<>();
-        int num = 0;
-        for (Integer i : treeSet) {
-            mapper.put(i, num);
-            num++;
-        }
-
-        int[] L = new int[num];
-        int[] R = new int[num];
-
-        Arrays.fill(L, -MAX_IDX);
-        Arrays.fill(R, MAX_IDX);
-
-        for (int station : stations) {
-            L[mapper.get(station)] = station;
-            R[mapper.get(station)] = station;
-        }
-
-        for (int i = 1; i < num; i++) {
-            L[i] = Math.max(L[i - 1], L[i]);
-        }
-        for (int i = num - 2; i >= 0; i--) {
-            R[i] = Math.min(R[i + 1], R[i]);
-        }
+        Arrays.sort(fires);
+        Arrays.sort(stations);
 
         int answer = 0;
-        for (int fire : fires) {
-            answer = Math.max(answer, Math.min(fire - L[mapper.get(fire)], R[mapper.get(fire)] - fire));
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j + 1 < m && getDist(i, j) > getDist(i, j + 1)) {
+                j++;
+            }
+            answer = Math.max(answer, getDist(i, j));
         }
         System.out.println(answer);
     }
 
+    static int getDist(int i, int j) {
+        return Math.abs(fires[i] - stations[j]);
+    }
 }
